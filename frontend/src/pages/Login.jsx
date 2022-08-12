cbe4a27fd4512523610deebb2daa9bf739bf6441
Royalboe <auomidejohnson@gmail.com>
2022-08-12 18:30:39 +0100

    Feat: Added the login page contents

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,11 +1,63 @@
-import React from 'react'
+import React, { useState } from "react";
+import { FaSignInAlt, FaSignOutAlt, FaUser } from "react-icons/fa";
 
 const Login = () => {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+  const { email, password } = formData;
+  const onChange = (e) =>
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  console.log(formData);
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <>
+      <section className="heading">
+        <h1>
+          <FaSignOutAlt />
+          Login
+        </h1>
+        <p>Please sign into your account</p>
+      </section>
+      <section className="form">
+        <form onSubmit={onSubmit}>
+          <div className="form-group">
+            <input
+              type="email"
+              className="form-control"
+              id="email"
+              name="email"
+              value={email}
+              placeholder="Enter your email address"
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              className="form-control"
+              id="password"
+              name="password"
+              value={password}
+              placeholder="Enter your password"
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+            <button type="Submit" className="btn btn-block">
+              Submit
+            </button>
+          </div>
+        </form>
+      </section>
+    </>
+  );
+};
 
-export default Login
+export default Login;
